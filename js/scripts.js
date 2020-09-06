f7a8b8970ef5f815e53f6dd0048129e03e3784e9
Adriana Rodriguez <agrodriguezs09@gmail.com>
2020-09-06 16:37:22 +0200

    cambios en los estilos

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 document.getElementById("cerrar").style.display = "none";
 function mostrar() {
     document.getElementById("navbar").style.width = "300px";
-  document.getElementById("navbar").style.display = "inline";
+    document.getElementById("navbar").style.display = "inline";
     document.getElementById("main-doc").style.marginLeft = "300px";
     document.getElementById("abrir").style.display = "none";
     document.getElementById("cerrar").style.display = "inline";
 }
 
 function ocultar() {
-  document.getElementById("navbar").style.display = "none";
+    document.getElementById("navbar").style.display = "none";
     document.getElementById("navbar").style.width = "0";
     document.getElementById("main-doc").style.marginLeft = "0";
     document.getElementById("abrir").style.display = "inline";
